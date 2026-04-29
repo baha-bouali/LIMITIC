38a8646b44eb23eb0156dc66e727be8684b9cccd
Firas Briki <firas.brikii12@gmail.com>
2026-04-29 23:15:15 +0100

    Change CreatedBy Type to GUID

--- LIMTIC.Service/LIMTIC.Domain/Shared/BaseEntity.cs
+++ LIMTIC.Service/LIMTIC.Domain/Shared/BaseEntity.cs
@@ -3,7 +3,7 @@
     public class BaseEntity
     {
         public Guid Id { get; set; }
-        public int CreatedBy { get; set; }
+        public Guid CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
---
+++ LIMTIC.Service/LIMTIC.Infrastructure/Data/Migrations/20260429220323_ChangeCreatedByType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LIMTIC.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCreatedByType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"ALTER TABLE ""Users"" ALTER COLUMN ""CreatedBy"" TYPE uuid USING (""CreatedBy""::text::uuid);"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"ALTER TABLE ""Users"" ALTER COLUMN ""CreatedBy"" TYPE integer USING (""CreatedBy""::text::integer);"
+            );
+            migrationBuilder.AlterColumn<int>(
+                name: "CreatedBy",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+    }
+}
