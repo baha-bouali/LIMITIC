b0def8e106d4a7af3a2439f9799141bafd73faa3
Adem Ksouri <ksouriadem44@gmail.com>
2026-05-25 07:17:27 +0100

    Add missing migration

---
+++ LIMTIC.Service/LIMTIC.Infrastructure/Data/Migrations/20260525061307_AddAuditLogsTable.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LIMTIC.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuditLogsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ResearchAxes_Users_UserId",
+                table: "ResearchAxes");
+
+            migrationBuilder.DropColumn(
+                name: "OTPTokenExpiry",
+                table: "ResearchAxes");
+
+            migrationBuilder.DropColumn(
+                name: "OTPTokenHash",
+                table: "ResearchAxes");
+
+            migrationBuilder.DropColumn(
+                name: "ResetPasswordTokenExpiry",
+                table: "ResearchAxes");
+
+            migrationBuilder.DropColumn(
+                name: "ResetPasswordTokenHash",
+                table: "ResearchAxes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "ResearchAxes");
+
+            migrationBuilder.AddColumn<string[]>(
+                name: "Authors",
+                table: "Publications",
+                type: "text[]",
+                nullable: false,
+                defaultValue: new string[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Venue",
+                table: "Publications",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Action = table.Column<string>(type: "text", nullable: false),
+                    Resource = table.Column<string>(type: "text", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreatedAtUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AuditLogs_Users_ActorId",
+                        column: x => x.ActorId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResetPasswords",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OTPTokenHash = table.Column<string>(type: "text", nullable: false),
+                    OTPTokenExpiry = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ResetPasswordTokenHash = table.Column<string>(type: "text", nullable: true),
+                    ResetPasswordTokenExpiry = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResetPasswords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResetPasswords_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "LabSettings",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2026, 5, 25, 6, 13, 6, 571, DateTimeKind.Utc).AddTicks(9367));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuditLogs_ActorId",
+                table: "AuditLogs",
+                column: "ActorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResetPasswords_UserId",
+                table: "ResetPasswords",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditLogs");
+
+            migrationBuilder.DropTable(
+                name: "ResetPasswords");
+
+            migrationBuilder.DropColumn(
+                name: "Authors",
+                table: "Publications");
+
+            migrationBuilder.DropColumn(
+                name: "Venue",
+                table: "Publications");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OTPTokenExpiry",
+                table: "ResearchAxes",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "OTPTokenHash",
+                table: "ResearchAxes",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ResetPasswordTokenExpiry",
+                table: "ResearchAxes",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResetPasswordTokenHash",
+                table: "ResearchAxes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "ResearchAxes",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.UpdateData(
+                table: "LabSettings",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2026, 5, 24, 0, 22, 22, 106, DateTimeKind.Utc).AddTicks(4560));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ResearchAxes_Users_UserId",
+                table: "ResearchAxes",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
